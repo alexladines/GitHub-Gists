b946ba1ca1ecbb3c17c82ff2d3067b3c6cad224a
alexladines <alexladines@outlook.com>
2019-08-22 09:14:27 -0700

    Make sure tableview loads sample data

--- GitHub Gists/View Controllers/GistsTableViewController.swift
+++ GitHub Gists/View Controllers/GistsTableViewController.swift
@@ -11,6 +11,7 @@ import Alamofire
 
 class GistsTableViewController: UITableViewController {
     // MARK: - Properties
+    var gists: [Gist] = []
 
     // MARK: - IBOutlets
 
@@ -22,23 +23,72 @@ class GistsTableViewController: UITableViewController {
         title = "Gists"
         navigationController?.navigationBar.prefersLargeTitles = true
 
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addGist))
         
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        GitHubAPIManager.shared.printPublicGists()
+        loadGists()
+        addSampleData()
     }
 
     // MARK: - Methods
+    func loadGists() {
+        GitHubAPIManager.shared.printPublicGists()
+    }
+
+    func addSampleData() {
+        let owner = Gist.Owner(login: "abc", avatarURL: URL(string: "https://hello.com"))
+        let gist1 = Gist(url: URL(string: "https://abc.com")!, id: "alexladines", gistDescription: "Hello World", owner: owner)
+        gists.append(gist1)
+        tableView.reloadData()
+    }
+
+    @objc func addGist() {
+        let alert = UIAlertController(title: "Not Implemented Yet", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
 
     // MARK: - Navigation
 
     // MARK: - Data Persistance
 
     // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gists.count
+    }
 
     // MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Gist", for: indexPath)
+
+        let gist = gists[indexPath.row]
+
+        cell.textLabel?.text = gist.gistDescription
+        cell.detailTextLabel?.text = gist.owner?.login
+
+        // TODO: set cell.imageView to display image at gist.ownerAvatarURL
+
+        return cell
+
+    }
+
+    // Cells will not be editable for now
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            gists.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        else if editingStyle == .insert {
+            // Create a new instance of the appropriate class,
+            // insert it into the array, and add a new row to the table view.
+        }
+    }
 
 }
