ae4460db8bf9fecd3e98abc76f7761325780979c
alexladines <alexladines@outlook.com>
2019-10-24 21:27:40 -0700

    Add documentation to BackendError.swift

--- GitHub Gists/Networking/BackendError.swift
+++ GitHub Gists/Networking/BackendError.swift
@@ -16,7 +16,7 @@ enum BackendError: Error {
     case apiProvidedError(reason: String)
     case authCouldNot(reason: String)
     case authLost(reason: String)
-    case missingRequiredInput(reason: String)
+    case missingRequiredInput(reason: String) //Add Documentation
 }
 
 // If endpoint fails, the API provides an error message in JSON form, we have to check for that in JSONDecoder
